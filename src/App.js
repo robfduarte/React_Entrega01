d02b8155a1ee9ff562c37774904ebaf9739345b2
Roberto Duarte <roberto.f.duarte@gmail.com>
2023-03-08 18:28:08 -0300

    clase0

--- src/App.js
+++ src/App.js
@@ -9,14 +9,18 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+    <h1>Hola comision 51150</h1>
+
+
+
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
