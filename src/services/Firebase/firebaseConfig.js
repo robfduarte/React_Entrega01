a572ddb7ede5ee973047e5c1cb41dc8543584cfa
Roberto Duarte <roberto.f.duarte@gmail.com>
2023-04-27 20:13:03 -0300

    version 1 - Entrega Final

--- src/services/Firebase/firebaseConfig.js
+++ src/services/Firebase/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBUVuFScRENjuHF1wZqQfr9J7B2tMFlJEU",
-  authDomain: "backend51150-d8c16.firebaseapp.com",
-  projectId: "backend51150-d8c16",
-  storageBucket: "backend51150-d8c16.appspot.com",
-  messagingSenderId: "531387617457",
-  appId: "1:531387617457:web:7718d7fab0a702833b1290"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 const app = initializeApp(firebaseConfig);
